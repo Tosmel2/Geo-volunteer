f812ce6244a74d06c14d137ebe9482af505ea321
tosmel2 <adewaletosin0808@gmail.com>
2022-08-25 23:15:44 +0100

    sushi food React app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Menu from './pages/Menu';
+import Cart from './pages/Cart';
+// import { Menu } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage/> } />
+          <Route path="/menu" element={<Menu/> } />
+          <Route path="/cart" element={<Cart/> } />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Carousel from '../components/Carousel'
+
+const Homepage = () => {
+  return (
+    <div className='home'>
+      <Carousel />
+    </div>
+  )
+}
+
+export default Homepage
