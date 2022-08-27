fc660bc5d1a3203e4babd771384272c694ad1270
tosmel2 <adewaletosin0808@gmail.com>
2022-08-27 17:21:02 +0100

    font awesome icon for react fixed

--- src/components/ServicesRendered.js
+++ src/components/ServicesRendered.js
@@ -7,8 +7,8 @@ const ServicesRendered = ({myImage, text, smallText}) => {
   return (
     <div className="renderSize" bg={useColorModeValue('gray.100', '#2B3252')}>
       {/* <img src={myImage} alt='mine' /> */}
-      <div style={{fontSize:'2rem', color:'#EF5455'}}>{myImage}</div>
-      <h2 style={{fontWeight:'bold', fontSize:'1.2rem'}}>{text}</h2>
+      <div style={{fontSize:'2rem', color:'#EF5455', marginBottom:'0'}}>{myImage}</div>
+      <h2 style={{fontWeight:'bold', fontSize:'1.2rem', color:'#000000'}}>{text}</h2>
       <p style={{color:'#000000'}}>{smallText}</p>
     </div>
   )
